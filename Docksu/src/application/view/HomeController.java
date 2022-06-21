a3c5342b887822867d769ff898bf306f1208b9b9
Furiza31 <hugo.wendjaneh@gmail.com>
2022-06-21 13:36:59 +0200

    Creation de la vue

--- Docksu/src/application/view/HomeController.java
+++ Docksu/src/application/view/HomeController.java
@@ -4,15 +4,40 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 import application.Main;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.control.TreeView;
 
 public class HomeController implements Initializable {
 	
 	private Main main;
+	
+	@FXML
+	TextField searchBar;
+	
+	@FXML
+	Button newFile;
+	
+	@FXML
+	Button newFolder;
+	
+	@FXML
+	TreeView<String> treeView;
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
+		newFile.setOnAction(event -> newFile());
+		newFolder.setOnAction(event -> newFolder());
+	}
 
+	private void newFolder() {
+		System.out.println("New folder");
+	}
+
+	private void newFile() {
+		System.out.println("New File");
 	}
 
 	/**
