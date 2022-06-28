8352aaed53db5b577797968b10ea2e5366b85562
Furiza31 <hugo.wendjaneh@gmail.com>
2022-06-28 23:17:51 +0200

    jsp

--- Docksu/src/application/view/HomeController.java
+++ Docksu/src/application/view/HomeController.java
@@ -3,7 +3,6 @@ package application.view;
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
-import java.util.ArrayList;
 import java.util.Objects;
 import java.util.ResourceBundle;
 
